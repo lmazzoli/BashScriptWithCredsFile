1af684a81ee2727df09c56a4a8293f9c51ffdde5
Leo Mazzoli <lmazzoli@us.ibm.com>
2018-11-19 16:33:36 -0500

    first commit

--- cfg.sh
+++ cfg.sh
@@ -10,12 +10,12 @@ if [ $region = "useast" ] ; then
   # US-East - API key
   credF=wa-credentials-api.txt
   url="https://gateway-wdc.watsonplatform.net/assistant/api"
-  waWorkspaceId=7d6e2a83-c637-41d5-abf0-c4d66a7beab2
+  waWorkspaceId=<WA_WORKSPACD_ID>
 else
   # US-South credentials - username:password
   credF=wa-credentials-dev.txt
   url="https://gateway.watsonplatform.net/assistant/api"
-  waWorkspaceId=7ac129fa-b33e-464f-b853-85cecec6a318
+  waWorkspaceId=<WA_WORKSPACD_ID>
 
 fi
 #
